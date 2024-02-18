f9fd6f1c73d228b1b5d7113592a345f46afbf96d
diiannn <dianastri75@gmail.com>
2024-02-18 17:34:46 +0700

    first commit

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,7 @@
+
+<?php 
+$string1 = "Topi";
+$string2 = "Bundar";
+
+echo  "<i><h2>" .'"' . "$string1 saya $string2, $string2 $string1 saya." . '"'. "</i></h2>";
+?>
---
+++ tugas/tugas1/1c..php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+    <style>
+        .kotak {
+            width: 70px;
+            height: 70px;
+            background-color: orange;
+            border: 2px solid black;
+            color: white;
+            display: flex;
+            
+        }
+
+        .kotak h3 {
+            margin: auto;
+        }
+
+        .row {
+            display: flex;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="row row-1">        
+        <div class="kotak kotak1"><h3>1</h3></div>
+    </div>
+    
+    <div class="row row-2">
+        <div class="kotak kotak2"><h3>2</h3></div>
+        <div class="kotak kotak2"><h3>2</h3></div>
+    </div>
+
+    <div class="row row-3">
+        <div class="kotak kotak3"><h3>3</h3></div>
+        <div class="kotak kotak3"><h3>3</h3></div>
+        <div class="kotak kotak3"><h3>3</h3></div>
+    </div>
+</body>
+</html>
